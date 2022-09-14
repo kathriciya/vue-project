d33d551b834212011fd650fe10996feb33bbdf3d
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-15 04:26:59 +0700

    create App

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button v-on:click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+    <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike(){
+      this.dislikes += 1;
+    }
+  }
+};
+</script>
+
+<style></style>
