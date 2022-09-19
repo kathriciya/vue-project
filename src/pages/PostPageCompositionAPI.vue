d2bc23de567073c867a3b56419e76c26d5edae96
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-19 08:38:06 +0700

    use compositionAPI, create hooks

--- src/pages/PostPageCompositionAPI.vue
+++ src/pages/PostPageCompositionAPI.vue
@@ -1,24 +1,17 @@
 <template>
   <div>
-    <h1>{{ likes }}</h1>
-    <button @click="addLike">add like</button>
-    <!-- <h1>Страница с постами</h1>
+    <h1>Страница с постами</h1>
     <my-input v-focus v-model="searchQuery" placeholder="Поиск ..." />
     <div class="app__btns">
-      <my-button @click="showDialog">Создать пост</my-button>
+      <my-button>Создать пост</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
     </div>
 
     <my-dialog v-model:show="dialogVisible">
-      <post-form @create="createPost" />
+      <post-form />
     </my-dialog>
-    <post-list
-      :posts="sortedAndSearchedPosts"
-      @remove="removePost"
-      v-if="!isPostsLoading"
-    />
+    <post-list :posts="sortedAndSearchedPosts" v-if="!isPostsLoading" />
     <div v-else>Идет загрузка постов ...</div>
-    <div v-intersection="loadMorePosts" class="observer"></div> -->
   </div>
 </template>
 
@@ -27,7 +20,9 @@ import PostForm from '@/components/PostForm';
 import PostList from '@/components/PostList';
 import MyButton from '@/components/UI/MyButton.vue';
 import MyDialog from '@/components/UI/MyDialog.vue';
-import { ref } from 'vue';
+import { usePosts } from '@/hooks/usePosts';
+import useSortedAndSearchedPosts from '@/hooks/useSortedAndSearchedPosts';
+import useSortedPosts from '@/hooks/useSortedPosts';
 export default {
   components: {
     PostList,
@@ -45,14 +40,18 @@ export default {
     };
   },
   setup(props) {
-    const likes = ref(0);
-    console.log('likes: ', likes);
-    const addLike = () => {
-      likes.value += 1;
-    };
+    const { posts, isPostsLoading, totalPages } = usePosts(10);
+    const { selectedSort, sortedPosts } = useSortedPosts(posts);
+    const { searchQuery, sortedAndSearchedPosts } =
+      useSortedAndSearchedPosts(sortedPosts);
     return {
-      likes,
-      addLike,
+      posts,
+      isPostsLoading,
+      totalPages,
+      selectedSort,
+      sortedPosts,
+      searchQuery,
+      sortedAndSearchedPosts,
     };
   },
 };
